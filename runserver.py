2575375354eb89eedffe271e002427e318c87193
Håkon Andreas Jensen <hakon@hokky.org>
2015-09-17 09:04:52 +0200

    Added hostip configuration

--- runserver.py
+++ runserver.py
@@ -1,10 +1,10 @@
 import os
 from sheetmusic import app
 
-
 def runserver():
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    port = int(os.environ.get('PORT', 8080))
+    hostip = os.environ.get('HOSTIP','127.0.0.1')
+    app.run(host=hostip, port=port)
 
 if __name__ == '__main__':
     runserver()
